473eabd55e2a3551a6dc8036319b20ee8c706ac4
Markus Mühlberger <markus@mmuehlberger.com>
2016-12-16 10:09:32 +0100

    Initial commit

---
+++ TutsplusCourses/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TutsplusCourses
+//
+//  Created by Markus Mühlberger on 16/12/2016.
+//  Copyright © 2016 Markus Mühlberger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
